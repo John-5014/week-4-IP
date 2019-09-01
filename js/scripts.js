6346840a67c68b8b08fc8582382d23ea8ccee1fa
John-5014 <kaiserjohn52@gmail.com>
2019-09-01 19:02:58 +0300

    add scripts.js for user-interface

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,29 @@ placeOrder.prototype.summary = function() {
 
   return "pizzasize: " + this.sizeChoice + ", crust:" + this.sizeCrust + ", toppings:" + this.sizeTopping;
 };
+
+
+//USER INTERFACE
+$("#submit").click(function() {
+  var size = $('#size').val();
+  var crust = $('#crust').val();
+  var toppings = [];
+  $("input[type=checkbox]:checked").each(function() {
+      toppings.push($(this).val());
+  });
+  console.log(toppings);
+
+  var newOrder = new placeOrder(size, crust, toppings);
+  console.log(newOrder);
+  $('ul#placeorderlist').text("" + newOrder.summary() + "");
+
+  var anotherTopping = 0;
+  for (var a = 0; a < toppings.length; a++) {
+      anotherTopping += parseInt(toppings[a]);
+  }
+
+  var total = parseInt(size) + parseInt(crust) + parseInt(anotherTopping);
+  console.log(total);
+  $("#total").text(total);
+});
 });
