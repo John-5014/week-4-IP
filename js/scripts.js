68a0fca2a5d065ece5eba9d1ad73ffa25719e420
John-5014 <kaiserjohn52@gmail.com>
2019-09-01 19:00:28 +0300

    add scripts.js for business logic first

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
     }
   });
 });
+$(document).ready(function(){
+  //BUSINESS LOGIC
+  function placeOrder(size, crust, toppings) {
+    this.sizeChoice = size;
+    this.sizeCrust = crust;
+    this.sizeTopping = toppings;
+
+
+}
+placeOrder.prototype.summary = function() {
+
+  return "pizzasize: " + this.sizeChoice + ", crust:" + this.sizeCrust + ", toppings:" + this.sizeTopping;
+};
+});
